2e1feb81c3c9828dd217e6fbb5997ff629fbae2a
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2023-10-24 19:25:22 +0200

    add multi OS for install

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 rsync -r -u files/ ~/
+
+# Detectar el sistema operativo
+OS="$(uname -s)"
+
+# Si el sistema operativo es Darwin (macOS)
+if [ "$OS" = "Darwin" ]; then
+  rsync -r -u mac_files/ ~/
+fi
+
 mkdir -p ~/.local/bin/
 rsync -r -u bin/ ~/.local/bin/
 
--- install_mac.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-rsync -r -u files/ ~/
-rsync -r -u mac_files/ ~/
-mkdir -p ~/.local/bin/
-rsync -r -u bin/ ~/.local/bin/
-
-echo "close and reopen"
